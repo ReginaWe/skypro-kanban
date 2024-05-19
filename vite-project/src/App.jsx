145e121b8ae60f7fcdcc0cb6cb080551e78d44e0
Acer <regina-sabitova-1998@mail.ru>
2024-05-19 21:29:32 +0500

    Changes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 import PopBrowse from "./components/popups/PopBrowse/PopBrowse";
 import PopExit from "./components/popups/PopExit/PopExit";
 import PopNewCard from "./components/popups/PopNewCard/PopNewCard";
+import { cardList } from "./data";
 
 function App() {
+  const [cards, setCards] = useState(cardList)
   return (
     <div className="wrapper">
       {/* <!-- pop-up start--> */} {/* Alt + Shift + A */}
@@ -13,8 +16,8 @@ function App() {
       <PopNewCard />
       <PopBrowse />
 
-      <Header />
-      <Main />
+      <Header setCards={setCards} cards={cards} />
+      <Main cardList={cards} />
     </div>
   );
 }
