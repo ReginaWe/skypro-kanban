cde975ef571ce9fcd3c7541fecbcde35c4729e90
Acer <regina-sabitova-1998@mail.ru>
2024-05-26 22:57:16 +0500

    работа над домашкой

--- vite-project/src/components/Card/Card.jsx
+++ vite-project/src/components/Card/Card.jsx
@@ -1,18 +1,12 @@
+import * as S from "./Card.styled"
 const Card = ({ topic, title, date }) => {
-  const colorList = {
-    "Web Design": "_orange",
-    Copywriting: "_purple",
-    Research: "_green",
-  }
-
-  const colorClass = colorList[topic] || "";
   return (
     <div className="cards__item">
       <div className="cards__card card">
         <div className="card__group">
-          <div className={`card__theme ${colorClass}`}>
-            <p className={colorClass}>{topic}</p>
-          </div>
+          <S.CardTopic $topicColor="_purple">
+            <S.TopicText topicColor="_purple">{topic}</S.TopicText>
+          </S.CardTopic>
           <a href="#popBrowse" target="_self">
             <div className="card__btn">
               <div></div>
